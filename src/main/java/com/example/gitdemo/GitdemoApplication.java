64d7e559951f322c3777c0049f13fa895851b96b
SXJ <issongxj@163.com>
2021-05-20 10:09:48 +0800

    第二次样例修改

--- src/main/java/com/example/gitdemo/GitdemoApplication.java
+++ src/main/java/com/example/gitdemo/GitdemoApplication.java
@@ -8,6 +8,8 @@ public class GitdemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GitdemoApplication.class, args);
+        System.out.println("Hello Sxj");
     }
+
 
 }
